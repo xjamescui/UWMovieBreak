f9a576333d940e9767ca026981fbac54d1d77af6
rachtakeuchi <takeuchi__@hotmail.com>
2014-07-19 16:27:12 -0400

    Created layout for search view

--- widgets/uw_MB/uw_MB.js
+++ widgets/uw_MB/uw_MB.js
@@ -1,1 +1,48 @@
-function uw_MB(userid, htmlId) { $(htmlId).html("<h1>uw_MBs Widget</h1>"); } 
+function uw_MB(userid, htmlId) {
+  "use strict";
+  var templates = {};
+
+
+  // View for search
+  var searchView = {
+    // Update our view
+    updateView: function(msg){
+      var t = "";
+      if (msg === "error") {
+        t = templates.error;
+      } else if (msg === "search"){
+        $("#uw_MB_content").html(templates.search);
+      }
+    },
+
+    initView: function () {
+      console.log("Initializing uw_MB_searchView");
+
+      /*
+       *  We want to initialize our search view here to begin with.
+       */
+      $("#uw_MB_content").html(templates.search);
+
+      /*
+       * Now we want to set the controller for the search button
+       * Get the input fields for searching and tell the model
+       * to get the corresponding movie results.
+       */
+      $("#uw_MB_searchButton").click(function (){
+        // Gather our variables
+        var genre = $("#uw_MB_genre").val();
+        var releaseDate = $("#uw_MB_releaseDate").val();
+        console.log("Genre: " + genre + " Release: " + releaseDate);
+      });
+    }
+  };
+
+  // Initialize our widget
+  console.log("Initializing uw_MB(" + userid + ", " + htmlId + ")");
+  portal.loadTemplates("widgets/uw_MB/templates.json",
+      function (t) {
+        templates = t;
+        $(htmlId).html(templates.baseHtml);
+        searchView.initView();
+      });
+}
